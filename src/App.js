03dad9128472b083d2ee4ae79ac7b934448a032b
naveed60 <advmiannaveedali@gmail.com>
2023-12-13 18:21:00 +0500

    new components added

--- src/App.js
+++ src/App.js
@@ -9,83 +9,86 @@ import TabButton from "./components/TabButton";
 import UserLogin from "./components/UserLogin";
 import Discount from "./components/Discount";
 import { EXAMPLES } from "./data";
-import Card2 from "./components/Card2";
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.js';
+import CoreConcept from './components/CoreConcepts.js';
+import Blog from './components/Blog.js';
 
-const App = () => {
-  const [discount, setDiscount]= useState('apply Discount')
-	function handleClick(discounted){
-		setDiscount(discounted);
-		console.log(discount);
-	}
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
 
-	const [selectedTopic, setSeletedTopic] = useState('components')
-	 function handleSelect(selectedButton){
-		setSeletedTopic(selectedButton);
-		console.log('app components executing');
-	}
-	const [user, setUser] = useState({
-    email: '',
-    password: '',
-    loggedIn: false,
-  });
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
 
-	const handleLogin = () => {
-    // TODO: Update user object with dummy data
-    // Dummy data can be any non-empty strings for email and password, and loggedIn set to true
-    setUser({
-      email: 'dummy@example.com',
-      password: 'dummyPassword',
-      loggedIn: true,
-    });
-	}
+  console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+  
+
 	
-	
-	return (
-		<div className="App ">
-			<PrimarySearchAppBar/>
-			<Greet name ="Naveed Ali" />
-			<Discount price={()=> handleClick('75')}/>
-			<div className="container-fluid d-flex justify-content-center  vh-100">
-				<section>
-					<menu>
-						<TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-						<TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-						<TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-						<TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-            {selectedTopic}
-					</menu>
-          <div id="tab-content"> 
-          
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-
-          </div>
-				</section>
-			</div>
-
-				
-		
-			<UserLogin user={user} onLogin={handleLogin} />
-				{/* Display user information */}
-				<div>
-					<h2>User Information</h2>
-					<p>Email: {user.email}</p>
-					<p>Password: {user.password}</p>
-					<p>Logged In: {user.loggedIn ? 'Yes' : 'No'}</p>
-				</div>
-			<div className="container-fluid d-flex justify-content-center align-items-center vh-100">
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
-			</div>
-		</div>
-	);
+    return (
+      <>
+        <PrimarySearchAppBar/>
+			  {/* <Greet name ="Naveed Ali" />  */}
+        <Header/>
+        <main>
+        {/* <Blog/> */}
+          <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+              {CORE_CONCEPTS.map((conceptItem) =>(
+                <CoreConcept key={conceptItem.title}{...conceptItem}/>
+              ))}
+            </ul>
+          </section>
+          <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+              <TabButton
+                isSelected={selectedTopic === 'components'}
+                onSelect={() => handleSelect('components')}
+              >
+                Components
+              </TabButton>
+              <TabButton
+                isSelected={selectedTopic === 'jsx'}
+                onSelect={() => handleSelect('jsx')}
+              >
+                JSX
+              </TabButton>
+              <TabButton
+                isSelected={selectedTopic === 'props'}
+                onSelect={() => handleSelect('props')}
+              >
+                Props
+              </TabButton>
+              <TabButton
+                isSelected={selectedTopic === 'state'}
+                onSelect={() => handleSelect('state')}
+              >
+                State
+              </TabButton>
+            </menu>
+            {tabContent}
+          </section>
+        </main>
+      </>
+    );
 };
 
 export default App;
