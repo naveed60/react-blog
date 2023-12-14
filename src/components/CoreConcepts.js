5739536a16a29f884c49a8d3c4cb85c7737defea
naveed60 <advmiannaveedali@gmail.com>
2023-12-14 15:40:00 +0500

    create a new separate component

--- src/components/CoreConcepts.js
+++ src/components/CoreConcepts.js
@@ -1,10 +1,15 @@
-export default function CoreConcept({ image, title, description }) {
-    return (
-      <li>
-          <img src={image} alt={title} />
-          <h3>{title}</h3>
-          <p>{description}</p>
-      </li>
-    );
-  }
+import CoreConcept from "./CoreConcept"
+import { CORE_CONCEPTS } from "../data"
+export default function CoreConcepts(){
+  return(
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) =>(
+          <CoreConcept key={conceptItem.title}{...conceptItem}/>
+        ))}
+      </ul>
+    </section>
+  )
 
+}
